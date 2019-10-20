a03f3ebde388d568b70600b4a4878ea0b921f8e1
Frank Bille-Stauner <github@frankbille.dk>
2019-10-20 15:35:43 +0200

    Updates

---
+++ precache-manifest.271a55f601a58967df2073bf79f16691.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4dceee9b76e06c37779ed9046233d79",
+    "url": "/jid-ui/index.html"
+  },
+  {
+    "revision": "ddd263581b9475c7b5e4",
+    "url": "/jid-ui/static/js/2.981e23b6.chunk.js"
+  },
+  {
+    "revision": "8b796610d22a07ec55e5",
+    "url": "/jid-ui/static/js/main.ffeb8dc9.chunk.js"
+  },
+  {
+    "revision": "a5c8b50c9aeae73495bc",
+    "url": "/jid-ui/static/js/runtime-main.17b7ecd6.js"
+  },
+  {
+    "revision": "c2e75483359fe276ab9c3aa3aae15d5e",
+    "url": "/jid-ui/static/media/logo_50x50.c2e75483.png"
+  }
+]);
