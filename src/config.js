cb71b5e382ba3de95177f37564684c4154469047
Frank Bille-Stauner <frank@forecast.it>
2020-06-16 21:36:39 +0200

    Use jid-api.billestauner.dk

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 import {Api} from "./api";
 
-export const api = new Api('http://localhost:4000');
+export const api = new Api('https://jid-api.billestauner.dk');
 export const newCountryTime = 30; // minutes old
