d85927ca6082c3d761c57fb05310a519692ab9d6
Frank Bille-Stauner <github@frankbille.dk>
2020-07-06 00:09:43 +0200

    Add sentry

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/react';
 import {App} from './App';
 import * as serviceWorker from './serviceWorker';
 import {Intl} from "./intl";
+
+Sentry.init({dsn: "https://73832ea57a124280a31e284c12c71f19@o416351.ingest.sentry.io/5310749"});
 
 ReactDOM.render(<Intl><App/></Intl>, document.getElementById('root'));
 
