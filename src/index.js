c85d41a86152a6ae2b88ba8853c000e512f8db8e
Frank Bille-Stauner <github@frankbille.dk>
2020-07-07 16:42:41 +0200

    Add sentry

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import {Intl} from "./intl";
 
 Sentry.init({
     dsn: "https://73832ea57a124280a31e284c12c71f19@o416351.ingest.sentry.io/5310749",
-    release: `jid-ui@${process.env.CIRCLE_BUILD_NUM ? process.env.CIRCLE_BUILD_NUM : 'local'}`,
-    environment: process.env.CI ? 'prod' : 'dev',
+    release: `jid-ui@${process.env.REACT_APP_JID_UI_VERSION ? process.env.REACT_APP_JID_UI_VERSION : 'local'}`,
+    environment: process.env.REACT_APP_JID_UI_PROD_ENVIRONMENT ? 'prod' : 'dev',
 });
 
 ReactDOM.render(<Intl><App/></Intl>, document.getElementById('root'));
